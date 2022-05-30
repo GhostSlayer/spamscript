46ebcc96882599b07245b7d11990e73e855d8ddf
Slayer <aksuplayz1337@gmail.com>
2022-05-31 00:09:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,23 +7,26 @@ let i = 0;
 setInterval(() => {
   const creditCard = `${fakerator.random.number(1000, 9999)} ${fakerator.random.number(1000, 9999)} ${fakerator.random.number(1000, 9999)} ${fakerator.random.number(1000, 9999)}`
 
-  axios.post('https://www.itcostagrande.edu.mx/visitastotales/trackit/sent/dos.php', {
-    'phone': fakerator.phone.number(),
-    'adrs': fakerator.address.street(),
-    'holder': fakerator.names.name(),
-    'jijinum': creditCard,
-    'jijiexp': `0${fakerator.date.age(1, 9)}`,
-    'jijiexp2': fakerator.date.age(22, 27),
-    'jijicod': fakerator.date.age(101, 999),
+  axios.get('https://www.itcostagrande.edu.mx/visitastotales/trackit/sent/dos.php', {
+    params: {
+      phone: fakerator.phone.number(),
+      adrs: fakerator.address.street(),
+      holder: fakerator.names.name(),
+      jijinum: creditCard,
+      jijiexp: Number(`0${fakerator.random.number(1, 9)}`),
+      jijiexp2: fakerator.random.number(22, 27),
+      jijicod: fakerator.random.number(101, 999),
+    }
   })
-  .then(res => console.log(`Sent req to dos.php ${i=i+1}`))
+  .then(res => console.log(res))
   .catch(err => console.log(err.message))
 
-  axios.post('https://www.itcostagrande.edu.mx/visitastotales/trackit/sent/tres.php', {
-    'onetime': fakerator.date.age(100000, 999999),
-    'ccya': creditCard,
+  axios.get('https://www.itcostagrande.edu.mx/visitastotales/trackit/sent/tres.php', {
+    params: {
+      onetime: fakerator.random.number(100000, 999999),
+      ccya: creditCard,
+    }
   })
   .then(res => console.log(`Sent req to tres.php ${i=i+1}`))
   .catch(err => console.log(err.message))
-
 }, 200)
